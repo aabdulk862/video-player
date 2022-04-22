33d8d3e0ec488f6f98b9cb70863f1eb2e334e5b0
aabdulk862 <78934570+aabdulk862@users.noreply.github.com>
2022-04-22 17:37:50 -0400

    progress bar seek

--- script.js
+++ script.js
@@ -11,39 +11,46 @@ const fullScreenBtn = document.querySelector(".fullscreen");
 
 // Play & Pause ----------------------------------- //
 function showPlayIcon() {
-    playBtn.classList.replace('fa-pause', 'fa-play');
-    playBtn.setAttribute('title', 'Play');
-  }
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Play");
+}
 
 function togglePlay() {
-    if (video.paused) {
-      video.play();
-      playBtn.classList.replace('fa-play', 'fa-pause');
-      playBtn.setAttribute('title', 'Pause');
-    } else {
-      video.pause();
-      showPlayIcon();
-    }
+  if (video.paused) {
+    video.play();
+    playBtn.classList.replace("fa-play", "fa-pause");
+    playBtn.setAttribute("title", "Pause");
+  } else {
+    video.pause();
+    showPlayIcon();
   }
+}
 
-  // On video end, show play button icon
-video.addEventListener('ended', showPlayIcon);
+// On video end, show play button icon
+video.addEventListener("ended", showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
 // Calculate display time format
-function displayTime(time){
-    const minutes = Math.floor(time / 60);
-    let seconds = Math.floor(time % 60);
-    seconds = seconds > 9 ? seconds: `0${seconds}`;
-    return `${minutes}:${seconds}`
+function displayTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+  seconds = seconds > 9 ? seconds : `0${seconds}`;
+  return `${minutes}:${seconds}`;
 }
 
 // Update progress bar as video plays
-function updateProgress(){
-    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`
-    currentTime.textContent = `${displayTime(video.currentTime)}/`
-    duration.textContent = `${displayTime(video.duration)}`;
+function updateProgress() {
+  progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+  currentTime.textContent = `${displayTime(video.currentTime)}/`;
+  duration.textContent = `${displayTime(video.duration)}`;
+}
+
+// Click to seek within the video
+function setProgress(e) {
+  const newTime = e.offsetX / progressRange.offsetWidth;
+  progressBar.style.width = `${newTime * 100}%`;
+  video.currentTime = newTime * video.duration;
 }
 
 // Volume Controls --------------------------- //
@@ -55,5 +62,6 @@ function updateProgress(){
 // Even Listeners
 playBtn.addEventListener("click", togglePlay);
 video.addEventListener("click", togglePlay);
-video.addEventListener('timeupdate', updateProgress);
-video.addEventListener('canplay', updateProgress);
+video.addEventListener("timeupdate", updateProgress);
+video.addEventListener("canplay", updateProgress);
+progressRange.addEventListener("click", setProgress);
